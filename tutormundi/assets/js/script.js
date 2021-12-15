749a23f77a914caec54ea86bee3aa8ba5daf3686
fscmatos <81788323+fscmatos@users.noreply.github.com>
2021-12-15 20:29:34 -0300

    Add files via upload

---
+++ tutormundi/assets/js/script.js
@@ -0,0 +1,21 @@
+const ratingStars = [...document.getElementsByClassName("rating_star")];
+
+function executeRating(stars) {
+  const starClassActive = "rating_star active";
+  const starClassInactive = "rating_star inactive";
+  const confirmClicked = "clicado";
+  const starsLength = stars.length;
+  let i;
+  stars.map((star) => {
+    star.onclick = () => {
+      i = stars.indexOf(star);
+      if (star.className === starClassInactive) {
+        for (i; i >= 0; --i) stars[i].className = starClassActive;
+      } else {
+        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+      }
+    document.getElementByClass("btn2").className = "confirmClicked";
+    };
+  });
+}
+executeRating(ratingStars);
